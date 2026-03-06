4a47e5092f82721ea7ad26138ac255359c5d5200
Homomorphism-of-H <faehupp@gmail.com>
2026-03-06 16:41:26 -0600

    Switch std::net to tokio::net

--- driad-server/src/main.rs
+++ driad-server/src/main.rs
@@ -1,6 +1,8 @@
 use std::error::Error;
-use std::io::Write;
-use std::net::{Ipv4Addr, TcpListener};
+use std::io::{Read, Write};
+use std::net::Ipv4Addr;
+use tokio::io::AsyncWriteExt;
+use tokio::net::{ TcpListener};
 
 use driad_common::SharedEntity;
 use hecs::World;
@@ -11,12 +13,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     world.spawn((SharedEntity,));
 
-    let listener = TcpListener::bind((Ipv4Addr::LOCALHOST, 7854))?;
+    let listener: TcpListener = TcpListener::bind((Ipv4Addr::LOCALHOST, 7854)).await?;
 
     'accept_connections: loop {
-        let (mut stream, addr) = listener.accept()?;
+        let (mut stream, addr) = listener.accept().await?;
         println!("{addr}");
-        stream.write_all(b"Hello!")?;
+        stream.write_all(b">>> ").await?;
         drop(stream);
     }
 
